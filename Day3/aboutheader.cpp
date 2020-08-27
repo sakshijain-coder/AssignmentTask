45fa2ceeb3c450f4f8b773b3aea56642e39aa619
Sakshi jain <63332968+sakshijain-coder@users.noreply.github.com>
2020-08-27 13:42:45 +0530

    Update aboutheader.cpp

--- Day3/aboutheader.cpp
+++ Day3/aboutheader.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include "myheader.h"
-#include "myheader.h"
+#include "myheader.h"  // i have added #pragma once in the header file that's why it will add only one time. if we don't add then this will give error!
 int main()
 {
 	int a = 10;
